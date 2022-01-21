a73d38c26990c0d7f57e8db8114e2ef27c5129db
Bhavanasg <bhavanaminnu@gmail.com>
2022-01-21 16:40:53 +0530

    new changes

--- script.js
+++ script.js
@@ -25,3 +25,94 @@ function loginvalidate()
     }
     
 };
+
+var signmail = document.getElementById("signemail");
+var signpswd = document.getElementById("password1");
+var signpswd2 = document.getElementById("password2");
+var signphone = document.getElementById("signphone");
+
+function signvalidate()
+{
+    var res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var psr = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    
+
+    if(res.test(signmail.value)!="1")
+    {
+        
+        window.alert("Email Is Invalid");
+        return false;
+    }
+    else if(phoneno.test(signphone.value)!="1")
+    {
+        window.alert("Please enter a valid phone number :");
+        return false;
+    }
+    else if(psr.test(signpswd.value)!="1")
+    {
+        window.alert("Password Is Invalid");
+        window.alert("Password should be of minimum 8 characters, atleast one uppercase,one lower case,one character and must contain at least one number");
+        return false;
+    }
+    else  if(signpswd.value!=signpswd2.value)
+    {
+        window.alert("Passwords Doesnt Match, Please Re-enter :");
+        return false;
+    }
+    else
+    {
+        window.alert("validation Success");
+        return true;
+    }
+    
+};
+
+
+
+let timeout;
+let password = document.getElementById('password1');
+let strengthBadge = document.getElementById('StrengthDisp');
+
+
+
+let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
+let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))');
+
+function StrengthChecker(PasswordParameter){
+    
+
+    if(strongPassword.test(PasswordParameter)) {
+        strengthBadge.style.backgroundColor = "green"
+        strengthBadge.textContent = 'Strong'
+    } else if(mediumPassword.test(PasswordParameter)){
+        strengthBadge.style.backgroundColor = 'orange'
+        strengthBadge.textContent = 'Medium'
+    } else{
+        strengthBadge.style.backgroundColor = 'red'
+        strengthBadge.textContent = 'Weak'
+    }
+};
+
+
+password.addEventListener("input", () => {
+
+    //The badge is hidden by default, so we show it
+
+    strengthBadge.style.display= 'block'
+    clearTimeout(timeout);
+
+    //We then call the StrengChecker function as a callback then pass the typed password to it
+
+    timeout = setTimeout(() => StrengthChecker(password.value), 500);
+
+    //Incase a user clears the text, the badge is hidden again
+
+    if(password.value.length !== 0){
+        strengthBadge.style.display != 'block'
+    } else{
+        strengthBadge.style.display = 'none'
+    }
+});
+
+
